7939460c943f0d2dd2cb884cb272f65586ca99b7
kukuwwka <ququwka1@gmail.com>
2023-11-27 21:49:36 +0400

    initial commit

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -6,8 +6,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources",
         glue = "org.example",
-        tags = "@success"
-
+        tags = "@successBookingProcess"
 )
 public class TestRunner {
 }
